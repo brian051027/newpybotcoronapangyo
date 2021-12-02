311b53d137eead67ef0840048ffb9766cfe231f5
brian051027 <70101429+brian051027@users.noreply.github.com>
2021-12-02 16:11:19 +0900

    Update Covid19KoreaStatusBot.py

--- Covid19KoreaStatusBot.py
+++ Covid19KoreaStatusBot.py
@@ -20,7 +20,7 @@ bottoken = ""
 
 @client.event # Use these decorator to register an event.
 async def on_ready(): # on_ready() event : when the bot has finised logging in and setting things up
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("!롤전적 (닉)or !롤체전적 (닉)을 처보세요."))
+    await client.change_presence(status=discord.Status.online, activity=discord.Game("!코로나를 처보세요."))
     print("New log in as {0.user}".format(client))
 
 @client.event
